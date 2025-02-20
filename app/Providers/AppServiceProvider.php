df18207153f7593c6dfc3516d0af69afeec5e9ce
juniyasyos <juniyasyos@gmail.com>
2025-02-20 20:57:13 +0700

    Initial commit for OAS project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use Filament\Support\Facades\FilamentView;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+        parent::register();
+        FilamentView::registerRenderHook('panels::body.end', fn(): string => Blade::render("@vite('resources/js/app.js')"));
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+
+        // Gate::policy()
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            $event->extendSocialite('discord', \SocialiteProviders\Google\Provider::class);
+        });
+
+        // Ngrok For Development
+        if (config('app.env') === 'local') {
+            URL::forceScheme('Https');
+        }
+    }
+}
